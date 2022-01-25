c4eb1e720ea32f9e3022dc871a3f0c6152815511
ondryice <ondryice@rams.colostate.edu>
2022-01-25 05:02:49 +0000

    skeleton btns

--- src/assets/components/margins/Navbar.js
+++ src/assets/components/margins/Navbar.js
@@ -5,15 +5,40 @@ export class Navbar extends React.Component {
         super(props);
 
         this.state = {
-
+            currentPage: this.props.currentPage
         };
+
+        this.sections = [
+            {   name:'home-page',
+                content: 'Home',
+                classes: 'navbar-btn-home',
+                onClick: null
+            }
+        ];
     }
 
     render() {
         return (
             <div className='navbar'>
                 <p>TODO: navbar content</p>
+                {this.renderButtons()}
             </div>
         );
+    }
+
+    renderButtons() {
+        return this.sections.map(
+            (section, index) => {
+                return (
+                    <div
+                        key={'nb-'+index}
+                        className={section.classes}
+                        onClick={section.onClick}
+                    >
+                        <p>{section.content}</p>
+                    </div>
+                );
+            }
+        )
     }
 }
